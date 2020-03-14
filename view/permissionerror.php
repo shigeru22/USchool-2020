99f3a7babca17eb6413181fc2021bf882081c615
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-03-14 22:58:58 +0700

    Add permission error page

---
+++ view/permissionerror.php
@@ -0,0 +1,16 @@
+<head>
+    <title>UTS Pemweb | Main</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/jquery-3.4.1.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body style="background-color: #fffcec;">
+    <div class="container text-center mt-5">
+        <i class="material-icons" style="font-size:96px;">error</i>
+        <h1>Permission Error</h1>
+        <p>You don't have the role to view this page.</p>
+        <a href="../main"><button class="btn btn-primary">Return to main page</button></a>
+    </div>
+</body>
