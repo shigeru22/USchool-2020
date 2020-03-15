e9aaf36b50f87a883c81a036f3406cc96e700a2b
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-03-15 13:12:18 +0700

    Add change students' grade frontend

---
+++ editgrade/index.php
@@ -0,0 +1,66 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <?php
+        // everything here is in session
+        if(isset($_SESSION["uschool-id"]) && isset($_SESSION["uschool-token"])) {
+            include "../include/db_connect.php";
+            include "../model/users.php";
+            include "../model/roles.php";
+            include "../model/grades.php";
+
+            $id = $_SESSION["uschool-id"];
+            $result = $db->query("SELECT * FROM user WHERE user_id='$id'");
+
+            if(mysqli_num_rows($result) == 1) {
+                $user = $result->fetch_assoc(); // user variable
+
+                if(md5($user["user_id"] . $user["password"]) == $_SESSION["uschool-token"]) {
+                    // logged in
+                    $currUser = new User($user["user_id"], $user["first_name"], $user["last_name"], $user["role_id"], $user["address"]);
+                    unset($user);
+
+                    $roles = array();
+                    $result = $db->query("SELECT * FROM role");
+                    while($row = $result->fetch_assoc()) {
+                        array_push($roles, new Role($row["role_id"], $row["role_name"], $row["role_desc"]));
+                    }
+
+                    if($roles[$currUser->getRoleId() - 1]->getName() == "guru") {
+                        $targetId = $_GET["id"];
+                        $result = $db->query("SELECT * FROM grade WHERE user_id='$targetId'");
+
+                        if(mysqli_num_rows($result) == 1) {
+                            $gradeQuery = $result->fetch_assoc();
+                            $grade = new Grade($gradeQuery["user_id"], $gradeQuery["nilai_tugas"], $gradeQuery["nilai_uts"], $gradeQuery["nilai_uas"]);
+                            unset($gradeQuery);
+                            
+                            $result = $db->query("SELECT * FROM user WHERE user_id='$targetId'");
+                            $userQuery = $result->fetch_assoc();
+                            $user = new User($userQuery["user_id"], $userQuery["first_name"], $userQuery["last_name"], $userQuery["role_id"], $userQuery["address"]);
+                            unset($userQuery);
+
+                            include "../view/editgrade.php";
+                        }
+                        else include "../view/usernotfound.php";
+                    }
+                    else include "../view/permissionerror.php";
+                }
+                else {
+                    echo "<script>window.location.href = \"..\"</script>";
+                }
+            }
+            else {
+                echo "<script>window.location.href = \"..\"</script>";
+            }
+        }
+        else {
+            echo "<script>window.location.href = \"..\"</script>";
+        }
+
+        mysqli_free_result($result);
+        mysqli_close($db);
+    ?>
+</html>
---
+++ view/editgrade.php
@@ -0,0 +1,142 @@
+<head>
+    <title>UTS Pemweb | Edit Student</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <script src="../assets/jquery-3.4.1.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script>
+        function editUser() {
+            $('#editBtn').attr("disabled", true);
+
+            var updated = 0;
+            $('#useridMsg').hide();
+            $('#fnameMsg').hide();
+            $('#lnameMsg').hide();
+            $('#roleMsg').hide();
+            $('#addrMsg').hide();
+            $('#unchangeErr').hide();
+            $('#queryErr').hide();
+            $('#phpErr').hide();
+
+            var userid = $('#userid').val();
+            var fname = $('#fname').val();
+            var lname = $('#lname').val();
+            var role = $('#role').val();
+            var address = $('#address').val();
+
+            if(userid == '') $('#useridMsg').show();
+            if(fname == '') $('#fnameMsg').show();
+            if(lname == '') $('#lnameMsg').show();
+            if(role == '') $('#roleMsg').show();
+            if(address == '') $('#addrMsg').show();
+
+            if(userid != '' && fname != '' && lname != '' && role != '' && address != '') {
+                $.ajax({
+                    url: "../controller/update_user.php",
+                    method: "POST",
+                    data: {
+                        userid: userid,
+                        fname: fname,
+                        lname: lname,
+                        role: role,
+                        address: address
+                    },
+                    success: function(data) {
+                        var result = JSON.parse(data);
+                        if(result.message == "success") {
+                            updated = 1;
+                            $('#updated').show();
+                            setTimeout(function(){
+                                window.location.href = "..";
+                            }, 3000);
+                        }
+                        else if(result.message == "unchanged") {
+                            $('#unchangeErr').show();
+                        }
+                        else if(result.message == "error") {
+                            $('#queryErr').show();
+                        }
+                    },
+                    error: function() {
+                        $('#phpErr').show();
+                    }
+                });
+            }
+
+            if(updated == 0) $('#addBtn').attr("disabled", false);
+        }
+    </script>
+</head>
+<body style="background-color: #fffcec;">
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ffe4b3;">
+        <div class="container">
+            <a class="navbar-brand" href="#">USchool</a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="navbar-collapse" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav ml-auto">
+                    <span class="navbar-text mr-2">
+                        <?php
+                            echo "Welcome, " . $currUser->getFName();
+                        ?>
+                    </span>
+                    <li class="nav-item">
+                        <button class="btn btn-danger">Logout</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container mt-4 col-sm-7 col-md-6 col-lg-5 col-xl-4">
+        <form id="addForm">
+            <div class="form-group">
+                <label for="fname">Nama</label>
+                <?php
+                    echo "<input type=\"text\" value=\"" . $user->getFName() . " " . $user->getLName() . "\" class=\"form-control\" id=\"fname\" disabled>";
+                ?>
+                <small id="fnameMsg" style="color:#ff0000; display: none;">Please enter this field.</small>
+            </div>
+            <div class="form-group">
+                <label for="userid">User ID</label>
+                <?php
+                    echo "<input type=\"text\" value=\"" . $_GET["id"] . "\" class=\"form-control\" id=\"userid\" disabled>";
+                ?>
+                <small id="useridMsg" style="color:#ff0000; display: none;">Please enter this field.</small>
+            </div>
+            <div class="form-group">
+                <label for="works">Nilai Tugas</label>
+                <?php
+                    echo "<input type=\"text\" value=\"" . $grade->getTugas() . "\" class=\"form-control\" id=\"works\">";
+                ?>
+                <small id="worksMsg" style="color:#ff0000; display: none;">Please enter this field.</small>
+            </div>
+            <div class="form-group">
+                <label for="midterm">Nilai UTS</label>
+                <?php
+                    echo "<input type=\"text\" value=\"" . $grade->getUTS() . "\" class=\"form-control\" id=\"midterm\">";
+                ?>
+                <small id="midtermMsg" style="color:#ff0000; display: none;">Please enter this field.</small>
+            </div>
+            <div class="form-group">
+                <label for="finalterm">Nilai UAS</label>
+                <?php
+                    echo "<input type=\"text\" value=\"" . $grade->getUAS() . "\" class=\"form-control\" id=\"finalterm\">";
+                ?>
+                <small id="finaltermMsg" style="color:#ff0000; display: none;">Please enter this field.</small>
+            </div>
+            <div class="text-center">
+                <p id="updated" style="color:#ff0000; display: none;">User updated. Redirecting to main page.</p>
+                <p id="unchangeErr" style="color: #ff0000; display: none;">Nothing changed.</p>
+                <p id="queryErr" style="color:#ff0000; display: none;">Query error or no permission. Please try again.</p>
+                <p id="phpErr" style="color:#ff0000; display: none;">An error occured. Please try again.</p>
+            </div>
+            <div class="row justify-content-center mb-4">
+                <button type="button" id="editBtn" class="btn btn-primary mr-4" onclick="editUser()">Update</button>
+                <?php
+                    echo "<button type=\"button\" id=\"cancelBtn\" class=\"btn btn-danger\" onclick=\"window.location.href = '../viewuser?id=" . $_GET["id"] . "'\">Cancel</button>"
+                ?>
+            </div>
+        </form>
+    </div>
+</body>
